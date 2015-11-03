3c59829deb14ed0c89cc99e7c05be32f2ac162cb
hbccdf <hbccdf@126.com>
2015-11-03 23:33:56 +0800

    Fix some compile errors about make_unique with vc++ 2015.

--- include/selene/util.h
+++ include/selene/util.h
@@ -91,7 +91,7 @@ inline int SetErrorHandler(lua_State *L) {
 }
 
 template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(Args&&... args) {
+std::unique_ptr<T> sel::make_unique(Args&&... args) {
     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 }
